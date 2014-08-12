0fe975ee0d98fedf1922341e999609ef0f3ff63c
Tom VanAntwerp <vanantwerp@gmail.com>
2014-08-12 12:04:01 -0400

    Ignore data folder, data moved to separate repo

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+// These configurations were used for local testing. Configure them as appropriate for your own setup.
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASSWORD','');
